a36a1262bc3ecff9ff31511f4fc0ae5450e7e07b
Pawel Kropiwnicki <pawlokro@gmail.com>
2024-07-21 14:15:21 +0200

    Add  detection of free falling default params value 100ms and +- 0.1g, Add signal lib and write a handlers to allow a user change parameters in run time

--- mcu/FallingDetector/main.c
+++ mcu/FallingDetector/main.c
@@ -4,12 +4,15 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <time.h>
+#include <signal.h>
 
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 65432
 #define BUFFER_SIZE 1024
 #define FREQUENCY_HZ 50
-#define DELAY_MS (1000 / FREQUENCY_HZ)
+#define DELAY_MS (1000 / FREQUENCY_HZ) // 20ms
+#define FF_DURATION_MS 100 // Default value 100ms
+#define FF_THRESHOLD_G 0.1 // Default value 0.1g for ax, ay, az
 
 typedef struct
 {
@@ -20,6 +23,28 @@ typedef struct
     double gy;
     double gz;
 }imu_data_t;
+
+typedef struct
+{
+    double duration;
+    double threshlod;
+}FF_detection_params_t;
+
+/*Local variable*/
+static volatile unsigned short FF_SetParamsFlag = 0;
+
+// Signal handler function
+void signalHandler(int signal) {
+
+    if (signal == SIGUSR1) {
+        printf("Received SIGUSR1 signal\n"); //debug
+        FF_SetParamsFlag = 1;
+    }
+    else if(signal == SIGTSTP){
+        printf("Received SIGTSTP signal, Press any button to continue\n"); //debug
+        FF_SetParamsFlag = 1;
+    }
+}
 
 void parseString(const char *pInputBuf, imu_data_t* pImuData) {
     // Use sscanf to parse the string into the three double variables
@@ -39,6 +64,16 @@ void delay(int milliseconds) {
 }
 
 int main() {
+
+    // Register signal handlers
+    signal(SIGUSR1, signalHandler);
+    signal(SIGTSTP, signalHandler);
+
+    printf("Process started. PID: %d\n", getpid());
+
+    // Infinite loop to keep the process running
+    printf("Running...To change the parameters of the FF detector Press Ctrl+Z to send SIGTSTP, or use 'kill -SIGUSR1 %d' to send SIGUSR1\n", getpid());
+
     int sockfd;
     struct sockaddr_in server_addr;
     char buffer[BUFFER_SIZE];
@@ -65,7 +100,10 @@ int main() {
     printf("Connected to server at %s:%d\n", SERVER_IP, SERVER_PORT);
 
     // Continuously send request and receive data from server at 50 Hz
-    imu_data_t imu_data = {0};
+    imu_data_t ImuData = {0};
+    FF_detection_params_t ImuConfig = {.duration = FF_DURATION_MS, .threshlod=FF_THRESHOLD_G};
+    int FF_start_sampling = 0;
+    int FreeFallActiveFlag = 0;
 
     while (1) {
         // Send request to server
@@ -78,7 +116,7 @@ int main() {
         bytes_received = recv(sockfd, buffer, BUFFER_SIZE - 1, 0);
         if (bytes_received > 0) {
             buffer[bytes_received] = '\0';
-            printf("%s", buffer);
+            // printf("%s", buffer); // debug
         } else if (bytes_received == 0) {
             printf("Connection closed by server\n");
             break;
@@ -87,15 +125,48 @@ int main() {
         }
 
         // Copy data from input buffor to struct imu_data
-        parseString(buffer,&imu_data);
-
-        if(MAIN_imu_data.ax == -0.8137455366679891)
+        parseString(buffer, &ImuData);
+        
+        //TODO swipe to a function
+        if ((ImuData.ax < ImuConfig.threshlod && ImuData.ax > -ImuConfig.threshlod) &&
+            (ImuData.ay < ImuConfig.threshlod && ImuData.ay > -ImuConfig.threshlod) &&
+            (ImuData.az < ImuConfig.threshlod && ImuData.az > -ImuConfig.threshlod))
         {
-            printf("Get correct value\n");
+            FF_start_sampling += DELAY_MS;
+
+            if(FF_start_sampling >= ImuConfig.duration && FreeFallActiveFlag == 0)
+            {
+                printf("Free fall: detected by imcu - Start \n");
+                FreeFallActiveFlag = 1;
+            }
         }
         else
         {
-            printf("Fail incorrect value\n");
+            if(FreeFallActiveFlag == 1)
+            {
+                printf("Free fall: detected by imcu - End \n");
+                FreeFallActiveFlag = 0;
+            }
+            FF_start_sampling = 0;
+        }
+
+        // Interrupt occured - TODO swipe to function that handle params change
+        if(FF_SetParamsFlag == 1)
+        {
+            int ChangeConfig=0;
+            printf("Present free fall detection settings:\n");
+            printf("Duration %lf [ms], threshlod = %lf [g]\n",ImuConfig.duration, ImuConfig.threshlod);
+            printf("Options: \nPress 1 to change settings\nPress 2 or any other keypad button to keep settings\n");
+            scanf("%d", &ChangeConfig);
+
+            if(ChangeConfig ==1)
+            {
+                do {
+                   printf("Type duration time and threshold value:");
+                } while (scanf("%lf %lf", &ImuConfig.duration, &ImuConfig.threshlod)!=2);
+            }
+
+            FF_SetParamsFlag = 0;
         }
 
         // Wait for the next interval
